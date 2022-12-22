48169696f477c3470c5dfc74ec4c37b472c0fcab
Misara Ahmed <84655733+Misara-Ahmed@users.noreply.github.com>
2022-12-22 23:43:10 +0200

    buttons

--- static/js/main.js
+++ static/js/main.js
@@ -1,92 +1,91 @@
-var rect = null;
-var cir = null;
-var line = null;
-var triangle = null;
-var isNowDrawing = false;
-let selectedbtn;
+let rect_flag=0;
+let cir_flag=0;
+let line_flag=0;
+let triangle_flag=0;
+let isNowDrawing = false;
+let btnId;
 
 let stage = new Konva.Stage({
-  container: "content",
+  container: 'content',
   width: 1100,
   height: window.innerHeight,
 });
-let layer1 = new Konva.Layer();
-let layer2 = new Konva.Layer();
-let layer3 = new Konva.Layer();
-let layer4 = new Konva.Layer();
-stage.add(layer1);
-stage.add(layer2);
-stage.add(layer3);
-stage.add(layer4);
+let layer = new Konva.Layer();
 
-function circleDown() {
-  cir = new Konva.Circle({
-    x: stage.getPointerPosition().x,
-    y: stage.getPointerPosition().y,
-    radius: 10,
-    fill: "red",
-    stroke: "black",
-    strokeWidth: 4,
-  });
-  layer1.add(cir).batchDraw();
+function circleDown(){
+    cir = new Konva.Circle({
+      x: stage.getPointerPosition().x,
+      y: stage.getPointerPosition().y,
+      radius: 10,
+      fill: 'red',
+      stroke: 'black',
+      strokeWidth: 4,
+    });
+    layer.add(cir);
+    layer.draw();
 }
 
-function circleMove() {
-  const rise = Math.pow(stage.getPointerPosition().y - cir.y(), 2);
-  const run = Math.pow(stage.getPointerPosition().x - cir.x(), 2);
-  const newRadius = Math.sqrt(rise + run);
+function circleMove(){
+  const rise=Math.pow(stage.getPointerPosition().y-cir.y(),2);
+  const run=Math.pow(stage.getPointerPosition().x-cir.x(),2);
+  const newRadius=Math.sqrt(rise+run);
   cir.radius(newRadius);
 }
 
-function rectdown() {
-  rect = new Konva.Rect({
-    x: stage.getPointerPosition().x,
-    y: stage.getPointerPosition().y,
-    width: 10,
-    height: 10,
-    fill: "red",
-    stroke: "black",
-    strokeWidth: 4,
-  });
-  layer2.add(rect).batchDraw();
+function rectdown(){
+    rect = new Konva.Rect({
+      x: stage.getPointerPosition().x,
+      y: stage.getPointerPosition().y,
+      width:10,
+      height:10,
+      fill: "red",
+      stroke: "black",
+      strokeWidth: 4 ,
+    });
+    // layer.add(rect).batchDraw()
+    layer.add(rect);
+    layer.draw();
 }
 
-function rectMove() {
-  const newWidth = stage.getPointerPosition().x - rect.x();
-  const newHeight = stage.getPointerPosition().y - rect.y();
-  rect.width(newWidth).height(newHeight);
+function rectMove(){
+  const newWidth=stage.getPointerPosition().x-rect.x();
+  const newHeight=stage.getPointerPosition().y-rect.y();
+  rect.width(newWidth);
+  rect.height(newHeight)
+
 }
 
-function triangleDown() {
+function triangleDown(){
   triangle = new Konva.RegularPolygon({
     x: stage.getPointerPosition().x,
     y: stage.getPointerPosition().y,
     sides: 3,
     radius: 20,
-    fill: "red",
-    stroke: "black",
+    fill: 'red',
+    stroke: 'black',
     strokeWidth: 4,
   });
-  layer3.add(triangle).batchDraw();
+  layer.add(triangle).batchDraw()
 }
 
-function triangleMove() {
-  const newWidth = stage.getPointerPosition().x - triangle.x();
-  const newHeight = stage.getPointerPosition().y - triangle.y();
-  triangle.width(newWidth).height(newHeight);
+function triangleMove(){
+  const newWidth=stage.getPointerPosition().x-triangle.x();
+  const newHeight=stage.getPointerPosition().y-triangle.y();
+  triangle.width(newWidth);
+  triangle.height(newHeight);
 }
 
-function lineDown() {
+function lineDown(){
   line = new Konva.Line({
-    stroke: "black",
+    stroke: 'black',
     // remove line from hit graph, so we can check intersections
     listening: false,
-    points: [stage.getPointerPosition().x, stage.getPointerPosition().y],
+    points: [ stage.getPointerPosition().x, stage.getPointerPosition().y]
   });
-  layer4.add(line).batchDraw();
+  layer.add(line).batchDraw()
 }
 
-function lineMove() {
+function lineMove(){
   const pos = stage.getPointerPosition();
   const points = line.points().slice();
   points[2] = pos.x;
@@ -94,107 +93,150 @@ function lineMove() {
   line.points(points);
 }
 
-function drawTriangle() {
-  stage.on("mousedown ", mousedownHandler);
-  stage.on("mousemove ", mousemoveHandler);
-  stage.on("mouseup ", mouseupHandler);
-  function mousedownHandler() {
-    isNowDrawing = true;
-    triangleDown();
-  }
-  function mousemoveHandler() {
-    if (!isNowDrawing) return false;
-    triangleMove();
-    //  layer.batchDraw();
-  }
-  function mouseupHandler() {
-    isNowDrawing = false;
-  }
-}
-
-function drawCircle() {
-  stage.on("mousedown ", mousedownHandler);
-  stage.on("mousemove ", mousemoveHandler);
-  stage.on("mouseup ", mouseupHandler);
-  function mousedownHandler() {
-    isNowDrawing = true;
-    circleDown();
-  }
-  function mousemoveHandler() {
-    if (!isNowDrawing) return false;
-    circleMove();
-    //layer.batchDraw();
-  }
-  function mouseupHandler() {
-    isNowDrawing = false;
-  }
-}
-
-function drawRect() {
-  stage.on("mousedown ", mousedownHandler);
-  stage.on("mousemove ", mousemoveHandler);
-  stage.on("mouseup ", mouseupHandler);
-  function mousedownHandler() {
-    isNowDrawing = true;
-    rectdown();
-  }
-  function mousemoveHandler() {
-    if (!isNowDrawing) return false;
-    rectMove();
-    //layer.batchDraw();
-  }
-  function mouseupHandler() {
-    isNowDrawing = false;
-  }
-}
-
-function drawLine() {
-  stage.on("mousedown ", mousedownHandler);
-  stage.on("mousemove ", mousemoveHandler);
-  stage.on("mouseup ", mouseupHandler);
-
-  function mousedownHandler() {
-    isNowDrawing = true;
-    lineDown();
-  }
-  function mousemoveHandler() {
-    if (!isNowDrawing) return false;
-    lineMove();
-  }
-  function mouseupHandler() {
-    isNowDrawing = false;
-  }
-}
-
-// if (selectedTool === "line") {
-//   console.log(selectedTool);
-//   drawLine();
-// } else if (selectedTool === "rectangle") {
-//   console.log(selectedTool);
-//   drawRect();
-// } else if (selectedTool === "circle") {
-//   console.log(selectedTool);
-//   drawCircle();
-// } else if (selectedTool === "triangle") {
-//   console.log(selectedTool);
-//   drawTriangle();
-// }
-const buttons = document.querySelectorAll(".button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    buttons.forEach((button) => button.classList.remove("active"));
-    button.classList.add("active");
-    selectedbtn = button.id;
-    console.log(btnId);
-    if (btnId === "rectangle") {
-      drawRect();
-    } else if (btnId === "circle") {
-      drawCircle();
-    } else if (btnId === "traingle") {
-      drawTriangle();
-    } else if (btnId === "line") {
-      drawLine();
+function drawTriangle(x){
+  triangle_flag = 1;
+  rect_flag=0;
+  cir_flag=0;
+  line_flag=0;
+  console.log("triangle")
+  stage.on('mousedown', mousedownHandler);
+  stage.on('mousemove', mousemoveHandler);
+  stage.on('mouseup', mouseupHandler);
+  function mousedownHandler(){
+    if (triangle_flag === 1) {
+      console.log("triangle")
+      isNowDrawing = true;
+      triangleDown();
     }
-  });
-});
+  }
+  function mousemoveHandler(){
+    if (triangle_flag === 1) {
+      if (!isNowDrawing) return false;
+      triangleMove();
+    }
+  //  layer.batchDraw();
+  }
+  function mouseupHandler() {
+    if (triangle_flag === 1) {
+      isNowDrawing = false;
+    }
+  }
+  stage.add(layer);
+}
+
+function drawCircle(){
+  cir_flag = 1;
+  rect_flag=0;
+  line_flag=0;
+  triangle_flag=0;
+  console.log("circle")
+  stage.on('mousedown circle', mousedownHandler);
+  stage.on('mousemove circle', mousemoveHandler);
+  stage.on('mouseup circle', mouseupHandler);
+  function mousedownHandler(){
+    if(cir_flag === 1) {
+      isNowDrawing = true;
+      circleDown();
+    }
+  }
+  function mousemoveHandler(){
+    if(cir_flag === 1) {
+      if (!isNowDrawing) return false;
+      circleMove();
+    }
+    //layer.batchDraw();
+  }
+  function mouseupHandler(){
+    if(cir_flag === 1) {
+      isNowDrawing = false;
+    }
+  }
+  stage.add(layer);
+}
+
+function drawRect(){
+  console.log("rect")
+  rect_flag = 1;
+  cir_flag=0;
+  line_flag=0;
+  triangle_flag=0;
+  stage.on('mousedown rect', mousedownHandler);
+  stage.on('mousemove rect', mousemoveHandler);
+  stage.on('mouseup rect', mouseupHandler);
+  function mousedownHandler(){
+    if(rect_flag === 1) {
+      console.log("rect")
+      isNowDrawing = true;
+      rectdown();
+    }
+  }
+  function mousemoveHandler(){
+    if(rect_flag === 1) {
+      if (!isNowDrawing) return false;
+      rectMove();
+    }
+    //layer.batchDraw();
+  }
+  function mouseupHandler(){
+    if(rect_flag === 1) {
+      console.log("rect")
+      isNowDrawing = false;
+    }
+  }
+  stage.add(layer);
+}
+
+function drawLine(){
+  console.log("line")
+  line_flag = 1;
+  rect_flag=0;
+  cir_flag=0;
+  triangle_flag=0;
+  stage.on('mousedown ', mousedownHandler);
+  stage.on('mousemove ', mousemoveHandler);
+  stage.on('mouseup ', mouseupHandler);
+
+  function mousedownHandler(){
+    if(line_flag === 1) {
+      console.log("line")
+      isNowDrawing = true;
+      lineDown();
+    }
+  }
+  function mousemoveHandler(){
+    if(line_flag === 1) {
+      if (!isNowDrawing) return false;
+      lineMove();
+    }
+  }
+  function mouseupHandler(){
+    if(line_flag === 1) {
+      console.log("line")
+      isNowDrawing = false;
+    }
+  }
+  stage.add(layer);
+}
+
+// const buttons = document.querySelectorAll('.button');
+//
+// buttons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     buttons.forEach(button => button.classList.remove('active'));
+//     button.classList.add('active');
+//     btnId = button.id;
+//     // console.log(btnId)
+//     if( btnId === 'rect') {
+//       drawRect();
+//     }
+//     if (btnId === 'circle'){
+//       drawCircle();
+//     }
+//     if(btnId === 'traingle'){
+//       drawTriangle(1);
+//     }
+//     if (btnId === 'line'){
+//       drawLine();
+//     }
+//     });
+// });
